420c9589e9e82a6b540ea6890e3a443a2f157b77
Mlowegene <genemlowe@gmail.com>
2024-01-17 20:52:41 +0300

    Feat: Add resume data and images

---
+++ src/resumeData.ts
@@ -0,0 +1,92 @@
+const resumeData = {
+  name: "Generation Mlowe",
+  role: "Frontend Web Developer ",
+  roleDescription:
+    "Highly motivated Front-End Developer with a passion for creating engaging and user-friendly web experiences. Proficient in HTML, CSS, and JavaScript, and eager to learn and master the latest front-end technologies and frameworks.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/generation-mlowe-441713240/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/Mlowegene",
+      className: "fa fa-github",
+    },
+    {
+      name: "Twitter",
+      url: "https://twitter.com/genemlowe",
+      className: "fa fa-twitter",
+    },
+  ],
+  aboutme:
+    "I am self Taught frontend developer based on React and Typescript, I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+  address: "Dar es salaam Tanzania",
+  education: [
+    {
+      UniversityName: "The University of Dodoma",
+      specialization: "Computer Information Security",
+      MonthOfPassing: "July",
+      YearOfPassing: "2021",
+      Achievements: "Awarded Degree",
+    },
+    {
+      UniversityName: "The University of Dodoma",
+      specialization: "Information and Communication Technology",
+      MonthOfPassing: "Dec",
+      YearOfPassing: "2017",
+      Achievements: "Awarded Diploma",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "AmarBiulders LTD",
+      specialization: "Frontend developer",
+      MonthOfLeaving: "April",
+      YearOfLeaving: "2023",
+      Achievements: "website development",
+    },
+  ],
+  skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "Reactjs",
+    },
+    {
+      skillname: "Typescript",
+    },
+    {
+      skillname: "Bootstrap",
+    },
+    {
+      skillname: "Tailwind",
+    },
+  ],
+  portfolio: [
+    {
+      name: "Github User Search",
+      appUrl: "/home/generation-mlowe/Pictures/Screenshots/git3.png",
+    },
+    {
+      name: "Netflix-Clone",
+      appUrl: "/home/generation-mlowe/Pictures/Screenshots/netflix.png",
+    },
+    {
+      name: "Dictionary-web-App",
+      appUrl: "https://mlowegene.github.io/dictionary-web-react-app/",
+    },
+    {
+      name: "Result-summary-component",
+      appUrl: "https://mlowegene.github.io/result-summary-component/",
+    },
+  ],
+};
+
+export default resumeData;
